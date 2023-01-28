70a36116a1c952bbe7479026e9363612824433d9
Rebecca Girndt <r.l.girndt@gmail.com>
2023-01-27 19:19:25 -0600

    started on post, gonna reinstall node to try and get nanoid to work

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const nanoid = require('nanoid');
 const path = require('path');
 const notes = require('./db/db.json');
 
@@ -10,7 +11,7 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) =>
+app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.js'))
 );
 
@@ -21,6 +22,31 @@ app.get('/notes', (req, res) =>
 app.get('/api/notes', (req, res) => {
     console.log(`${req.method} request recieved`);
     res.status(200).json(notes);
+});
+
+// `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved.
+
+app.post('/api/notes', (req, res) => {
+    console.log(`${req.method} note request recieved`);
+
+    // const { title, text } = req.body;
+
+    // const newNote = {
+    //     title,
+    //     text,
+    //     note_id: nanoid()
+    // };
+    const newNote = req.body.push(`note_id: ${nanoid()}`);
+
+    const response = {
+        status: 'Success',
+        body: newNote
+    };
+
+    console.log(response);
+
+    res.status(200).json(response);
+    console.log(res);
 });
 
 app.listen(PORT, () =>
